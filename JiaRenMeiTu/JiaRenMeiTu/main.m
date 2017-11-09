e2f975d761a805e39be52009487d5e7b66ee930f
ALittleNasty <379643671@qq.com>
2017-11-09 15:05:35 +0800

    add third library

---
+++ JiaRenMeiTu/JiaRenMeiTu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JiaRenMeiTu
+//
+//  Created by ALittleNasty on 2017/11/9.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
