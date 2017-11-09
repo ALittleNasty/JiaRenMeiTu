5b07278f371bd26ec9e34ac4839b4498f9f0c55f
ALittleNasty <379643671@qq.com>
2017-11-09 17:51:12 +0800

    configure launch controller

--- JiaRenMeiTu/Classes/Controller/MTLaunchViewController.m
+++ JiaRenMeiTu/Classes/Controller/MTLaunchViewController.m
@@ -4,11 +4,21 @@
 //
 //  Created by ALittleNasty on 2017/11/9.
 //  Copyright © 2017年 ALittleNasty. All rights reserved.
-//
 
-#import "MTLaunchViewController.h"
+// controller
+#import "MTLaunchViewController.h" // 启动页
+#import "MTHomeViewController.h"   // 主页
+
+// appDelegate
+#import "AppDelegate.h"
+
+// third
+#import <Masonry/Masonry.h>
 
 @interface MTLaunchViewController ()
+
+/** 广告图 */
+@property (nonatomic, strong) UIImageView  *adImageView;
 
 @end
 
@@ -17,8 +27,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
     
-    self.view.backgroundColor = [UIColor orangeColor];
+    _adImageView = [[UIImageView alloc] init];
+    _adImageView.image = [UIImage imageNamed:@"bg_02.jpg"];
+    [self.view addSubview:_adImageView];
+    [_adImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [self switchRootViewControllerAnimated];
+    });
+}
+
+#pragma mark - 动画切换根视图
+
+- (void)switchRootViewControllerAnimated
+{
+    [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionTransitionCrossDissolve animations:^{
+        
+        self.adImageView.alpha = 0.4;
+    } completion:^(BOOL finished) {
+        if (finished) {
+            
+            MTHomeViewController *homeVC = [[MTHomeViewController alloc] init];
+            AppDelegate *delegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+            [delegate.window setRootViewController:homeVC];
+        }
+    }];
 }
 
 @end
